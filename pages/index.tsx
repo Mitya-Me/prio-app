a42703bf29205b3fdbbe848d4915cdda2ab6eb5a
mitya_me <dmitrymefodyev@gmail.com>
2022-01-11 20:26:15 +0300

    add useContext (AppContext) and basic display for Sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,12 +38,7 @@ function Home({menu}: HomeProps): JSX.Element {
             </Tag>
             <Tag color='green'>small</Tag>
             <Rating rating={rating} isEditable setRating={setRating} />
-            <ul>
-                {menu.map(m => (
-                    <li key={m._id.secondCategory}> { m._id.secondCategory}</li>
-            ))}
-
-            </ul>
+            
         </>
     );
 }
