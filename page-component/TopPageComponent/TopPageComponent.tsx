0ddb88235649ef554238ada02438b5aae8c94844
mitya_me <dmitrymefodyev@gmail.com>
2022-01-16 22:42:05 +0300

    Add TopPageComponent

--- page-component/TopPageComponent/TopPageComponent.tsx
+++ page-component/TopPageComponent/TopPageComponent.tsx
@@ -1,8 +1,35 @@
-import { TopPageComponentProps } from "./TopPageComponent.props"
+import { Card, Htag, Tag } from "../../components";
+import { TopPageComponentProps } from "./TopPageComponent.props";
+import styles from "./TopPageComponent.module.css";
+import { HhData } from '../../components/HhData/hhData'
+import { TopLevelCategory } from '../../interfaces/page.interface';
 
+export const TopPageComponent = ({
+    page,
+    products,
+    firstCategory,
+}: TopPageComponentProps): JSX.Element => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.title}>
+                <Htag tag='h1'>{page.title}</Htag>
+                {products && <Tag color='gray' size='m'>
+                    {products.length}
+                </Tag>}
+                <span> Сортировка</span>
+            </div>
+			{products && products.map((p) =>
+				<div key={p._id}> {p.title} </div>)
+			}
 
-export const TopPageComponent = ({ page, products, firstCategory}:TopPageComponentProps): JSX.Element=> { 
-	return (
-		<>{products && products.length}</>
-	) 
-}
+            <div className={styles.hhTitle}>
+                <Htag tag='h2'> Вакансии - {page.category} </Htag>
+                <Tag color='red' size='m'>
+                    hh.ru
+                </Tag>
+			</div>
+
+            {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh}/>}
+        </div>
+    );
+};
