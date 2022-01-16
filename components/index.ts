717a02ad230be0230ea24fa880f1207b8edab971
mitya_me <dmitrymefodyev@gmail.com>
2022-01-16 22:44:14 +0300

    Add Card and HhData from index

--- components/index.ts
+++ components/index.ts
@@ -3,4 +3,7 @@ export * from './Ptag/Ptag'
 
 export * from './Button/Button'
 export * from './Tag/Tag'
+
 export * from './Rating/Rating'
+export * from './Card/Card'
+export * from './HhData/HhData'
