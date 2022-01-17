826b1138433607b66106e7994046d7aa6cc4beb7
mitya_me <dmitrymefodyev@gmail.com>
2022-01-17 04:28:36 +0300

    Add Search

---
+++ components/Search/Search.tsx
@@ -0,0 +1,47 @@
+import { SearchProps } from "./Search.props";
+import styles from './Search.module.css';
+import cn from 'classnames';
+import { Button } from './../Button/Button';
+import { Input } from './../Input/Input';
+import { useState } from "react";
+import GlassIcon from './glass.svg'
+import { useRouter } from "next/router";
+
+export const Search = ({ className, ...props }: SearchProps): JSX.Element => { 
+	const [search, setSearch] = useState<string>('')
+	const router = useRouter()
+
+	const gotToSearch = () => { 
+		router.push({
+			pathname: '/search/',
+			query: {
+				q: search
+			}
+		})
+	}
+
+	const handleKeyDown = (e: KeyboardEvent) => { 
+		if (e.key == 'Enter') { 
+			gotToSearch()
+		}
+	}
+
+	return (
+		<div className={cn(className, styles.search)} {...props}>
+			<Input
+				className={styles.input}
+				placeholder="Search..."
+				value={search}
+				onChange={e => setSearch(e.target.value)}
+				onKeyDown={handleKeyDown}	
+			/>
+			<Button
+				appearance="primary"
+				className={styles.button}
+				onClick={gotToSearch}
+			>
+				<GlassIcon/>
+			</Button>
+		</div>
+	)
+}
--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -3,12 +3,13 @@ import styles from './Sidebar.module.css';
 import { Menu } from "../Menu/Menu";
 import Logo from '../logo.svg'
 import cn  from 'classnames';
+import { Search } from './../../components/Search/Search';
 
 export const Sidebar = ({className, ...props}: SidebarProps) => { 
 	return (
 		<div className={cn(className, styles.sidebar)} {...props}>
 			<Logo className={styles.logo}/>	
-			<div> SEARCH </div>
+			<Search/>
 			<Menu/>
 		</div>
 	)
