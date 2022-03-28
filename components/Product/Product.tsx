1360debd336c18aea36b3fe444fee1bc4176118c
mitya_me <dmitrymefodyev@gmail.com>
2022-03-28 23:37:05 +0300

    JSX template for Product component

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,11 +1,22 @@
 import { ProductProps } from "./Product.props";
 import styles from './Product.module.css';
 import cn from 'classnames';
+import { Card } from "../Card/Card";
+import { Rating } from "../Rating/Rating";
+import { Tag } from "../Tag/Tag";
 
 export const Product = ({product, className, ...props}: ProductProps): JSX.Element => { 
 	return (
-		<div>
-			{product.title}
-		</div>
+		<Card>
+			<div className={styles.logo}><img src={ product.image } alt={product.title}/></div>
+			<div className={styles.title}> {product.title}</div>
+			<div className={styles.title}> {product.price}</div>
+			<div className={styles.credit}> {product.credit}</div>
+			<div className={styles.rating}> <Rating rating={product.reviewAvg ?? product.initialRating}/></div>
+			<div className={styles.tags}> {product.categories.map(c => <Tag key={c} color='ghost'></Tag>)} </div>
+			<div className={styles.priceTitle}> цена </div>
+			<div className={styles.creditTitle}> кредит </div>
+			<div className={styles.rateTitle}> { product.reviewCount} отзывов </div>
+		</Card>
 	)
 }
