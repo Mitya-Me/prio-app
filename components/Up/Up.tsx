76c57b7bb80f1af70163e5a3638a35684fad793c
mitya_me <dmitrymefodyev@gmail.com>
2022-04-06 20:10:52 +0300

    Add ButtonIcon component

--- components/Up/Up.tsx
+++ components/Up/Up.tsx
@@ -1,6 +1,7 @@
 import { motion, useAnimation } from 'framer-motion';
 import { useEffect } from 'react';
 import { useScrollY } from '../../hooks/useScrollY';
+import { ButtonIcon } from '../ButtonIcon/ButtonIcon';
 import styles from './Up.module.css';
 import UpIcon from './up.svg'
 
@@ -17,13 +18,12 @@ export const Up = (): JSX.Element => {
 	}
 
 	return (
-		<motion.button
+		<motion.div
 			className={styles.up}
-			onClick={scrollToTop}
 			animate={controls}
 			initial={{opacity: 0}}
 		>
-			<UpIcon/>
-		</motion.button>
+			<ButtonIcon onClick={scrollToTop}  appearance='primary' icon='up' />
+		</motion.div>
 	)
 }
